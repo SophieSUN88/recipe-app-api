64afc96a7c527d942cde48abc892047b58ebdd41
SophieSUN88 <85892147+SophieSUN88@users.noreply.github.com>
2022-07-29 00:06:46 -0400

    admin

--- app/app/settings.py
+++ app/app/settings.py
@@ -130,3 +130,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#user the User in core, as the customised model
+AUTH_USER_MODEL = "core.User"
